1922cad279b297c47c6a48103164af6c0ebf9ae0
aask42 <amelia.leah.w@gmail.com>
2025-05-11 00:22:18 -0700

    kinda working

--- radio_ctrl/radio_ctrl.c
+++ radio_ctrl/radio_ctrl.c
@@ -1,31 +1,58 @@
 #include "radio_ctrl.h"
 #include <fm_rda5807.h>
-#include <rds_parser.h>
+#include <librdsparser.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <stdint.h>
+#include <time.h>
 
 #define COUNT_OF(arr) (sizeof(arr) / sizeof((arr)[0]))
 #ifndef MIN
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #endif
 
-#define RSSI_THRESHOLD 60 // Minimum RSSI to consider a station valid
-#define MAX_STATIONS 10   // Maximum number of stations to store
+
+// define ANSI colours
+#define RED "\033[1;31m"
+#define WHITE "\033[37m"
+#define YELLOW "\033[93m"
+#define BOLD "\033[1m"
+#define ENDC "\033[m"
+
+
+#define RSSI_THRESHOLD (60) // Minimum RSSI to consider a station valid
+#define MAX_STATIONS (10)   // Maximum number of stations to store
 
 static const float STATION_PRESETS[] = {
     88.1f, 90.3f, 91.7f, 95.7f, 101.1f, 107.3f,
 };
+
 static_assert(COUNT_OF(STATION_PRESETS) <= 9, "");
 
 #define DEFAULT_FREQUENCY STATION_PRESETS[0]
 #define FM_CONFIG fm_config_usa()
 
 static rda5807_t radio;
-static rds_parser_t rds_parser;
+static rdsparser_t *rds_parser;
+
+// Store CT data from callback
+static struct {
+    uint16_t year;
+    uint8_t month;
+    uint8_t day;
+    uint8_t hour;
+    uint8_t minute;
+    bool valid;
+} ct_data = {0};
 
 static float station_list[MAX_STATIONS];
 static int station_count = 0;
+
+// RDS callback function prototypes
+static void callback_ps(rdsparser_t *rds, void *user_data);
+static void callback_rt(rdsparser_t *rds, rdsparser_rt_flag_t flag, void *user_data);
+static void callback_ct(rdsparser_t *rds, const rdsparser_ct_t *ct, void *user_data);
 
 static void print_help(void);
 static void update_rds(void);
@@ -61,7 +88,33 @@ bool radio_init(i2c_inst_t *i2c, uint sda_pin, uint scl_pin,
     fm_set_volume(&radio, MIN(start_volume, 30));
     fm_set_mute(&radio, false);
 
-    rds_parser_reset(&rds_parser);
+    // Initialize RDS parser
+#ifdef RDSPARSER_DISABLE_HEAP
+    rdsparser_t buffer;
+    rdsparser_init(&buffer);
+    rds_parser = &buffer;
+#else
+    rds_parser = rdsparser_new();
+    if (rds_parser == NULL) {
+        printf("Error: Failed to create RDS parser.\n");
+        return false;
+    }
+#endif
+
+    // Configure RDS parser
+    rdsparser_set_text_correction(rds_parser, RDSPARSER_TEXT_PS, RDSPARSER_BLOCK_TYPE_INFO, RDSPARSER_BLOCK_ERROR_LARGE);
+    rdsparser_set_text_correction(rds_parser, RDSPARSER_TEXT_PS, RDSPARSER_BLOCK_TYPE_DATA, RDSPARSER_BLOCK_ERROR_LARGE);
+    rdsparser_set_text_correction(rds_parser, RDSPARSER_TEXT_RT, RDSPARSER_BLOCK_TYPE_INFO, RDSPARSER_BLOCK_ERROR_LARGE);
+    rdsparser_set_text_correction(rds_parser, RDSPARSER_TEXT_RT, RDSPARSER_BLOCK_TYPE_DATA, RDSPARSER_BLOCK_ERROR_LARGE);
+    
+    rdsparser_set_text_progressive(rds_parser, RDSPARSER_TEXT_PS, true);
+    rdsparser_set_text_progressive(rds_parser, RDSPARSER_TEXT_RT, true);
+    
+    // Register callbacks
+    rdsparser_register_ps(rds_parser, callback_ps);
+    rdsparser_register_rt(rds_parser, callback_rt);
+    rdsparser_register_ct(rds_parser, callback_ct);
+    
     printf("Radio Running Successfully!\n");
 
     return true;
@@ -71,6 +124,13 @@ void radio_deinit(void)
 {
     if (fm_is_powered_up(&radio))
         fm_power_down(&radio);
+        
+#ifndef RDSPARSER_DISABLE_HEAP
+    if (rds_parser != NULL) {
+        rdsparser_free(rds_parser);
+        rds_parser = NULL;
+    }
+#endif
 }
 
 void radio_feed_char(int ch)
@@ -196,19 +256,17 @@ uint8_t radio_get_rssi(void) { return fm_get_rssi(&radio); }
 
 static void update_rds(void)
 {
-    union {
-        uint16_t group_data[4];
-        rds_group_t group;
-    } rds;
-
-    if (fm_read_rds_group(&radio, rds.group_data)) {
-        rds_parser_update(&rds_parser, &rds.group);
+    rdsparser_data_t data;
+    rdsparser_error_t errors = {0}; // No error information from RDA5807
+    
+    if (fm_read_rds_group(&radio, data)) {
+        rdsparser_parse(rds_parser, data, errors);
     }
 }
 
 static void print_help(void)
 {
-    printf("Radio Control Menu:\n");
+    printf(YELLOW "Radio Control Menu:" ENDC "\n");
     printf("====================\n");
     printf("- =   Volume down / up\n");
     printf("s     Scan for stations\n");
@@ -217,13 +275,13 @@ static void print_help(void)
     printf("[ ]   Seek down / up\n");
     printf("< >   Adjust seek threshold\n");
     printf("0     Toggle mute\n");
-    printf("f     Toggle softmute\n");
+    printf("f     Toggle soft mute\n");
     printf("m     Toggle mono\n");
     printf("b     Toggle bass boost\n");
     printf("i     Print station info\n");
     printf("r     Print RDS info\n");
     printf("x     Power down / up\n");
-    printf("?     Print this help menu\n");
+    printf(BOLD "?     Print this help menu" ENDC "\n");
 }
 
 static void scan_stations(void)
@@ -259,55 +317,120 @@ static void print_station_info(void)
     printf("Signal Strength (RSSI): %u\n", fm_get_rssi(&radio));
 }
 
-// Add these helper functions
-static void mjd_to_date(uint32_t mjd, uint16_t *year, uint8_t *month, uint8_t *day) {
-    uint32_t j = mjd + 2400001;
-    uint32_t y = (uint32_t)((j - 1867216.25)/36524.25);
-    uint32_t c = j + y - (y/4) + 1525;
-    y = (uint32_t)((c - 122.1)/365.25);
-    uint32_t d = (uint32_t)(365.25 * y);
-    uint32_t m = (uint32_t)((c - d)/30.6001);
+// RDS callback functions
+static void callback_ps(rdsparser_t *rds, void *user_data)
+{
+    // Program Service Name updated
+    // This is called automatically when PS changes
+}
+
+static void callback_rt(rdsparser_t *rds, rdsparser_rt_flag_t flag, void *user_data)
+{
+    // Radio Text updated
+    // This is called automatically when RT changes
+}
+
+void format_rds_time(const rdsparser_ct_t *ct, char *buffer, size_t buffer_size) {
+    // Create a tm structure from the RDS CT data
+    struct tm timeinfo = {0};
+    timeinfo.tm_year = rdsparser_ct_get_year(ct) - 1900;  // tm_year is years since 1900
+    timeinfo.tm_mon = rdsparser_ct_get_month(ct) - 1;     // tm_mon is 0-11
+    timeinfo.tm_mday = rdsparser_ct_get_day(ct);
+    timeinfo.tm_hour = rdsparser_ct_get_hour(ct);
+    timeinfo.tm_min = rdsparser_ct_get_minute(ct);
+    timeinfo.tm_sec = 0;                 // RDS CT doesn't provide seconds
     
-    *day = c - d - (uint32_t)(30.6001 * m);
-    *month = m - 1;
-    if (m > 13) *month = m - 13;
-    *year = y - 4715;
-    if (*month > 2) *year -= 1;
+    // Format the time
+    strftime(buffer, buffer_size, "%Y-%m-%d %H:%M:%S", &timeinfo);
+}
+
+
+static void callback_ct(rdsparser_t *rds, const rdsparser_ct_t *ct, void *user_data)
+{
+    int16_t offset = rdsparser_ct_get_offset(ct);
+    char time_str[64];
+    format_rds_time(ct, time_str, sizeof(time_str));
+    
+    printf("Formatted time: %s\n", time_str);
+    printf("UTC offset: %d half-hours\n", offset);
 }
 
 
 static void print_rds_info(void)
 {
     printf("RDS Info:\n");
-    char program_id_str[5];
-    rds_get_program_id_as_str(&rds_parser, program_id_str);
-    printf("RDS - PI: %s, PTY: %u, DI_PTY: %u, DI_ST: %u, MS: %u, TP: %u, TA: %u\n",
-        program_id_str,
-        rds_get_program_type(&rds_parser),
-        rds_has_dynamic_program_type(&rds_parser),
-        rds_has_stereo(&rds_parser),
-        rds_has_music(&rds_parser),
-        rds_has_traffic_program(&rds_parser),
-        rds_has_traffic_announcement(&rds_parser));
-    printf("      PS: %s\n", rds_get_program_service_name_str(&rds_parser));
     
-    // Add clock time and date display
-    uint8_t hour, minute;
-    if (rds_get_time(&rds_parser, &hour, &minute)) {
-        uint16_t year;
-        uint8_t month, day;
-        mjd_to_date(rds_parser.time_info.mjd, &year, &month, &day);
-        printf("   Date: %02u/%02u/%04u\n", day, month, year);
-        printf("   Time: %02u:%02u\n", hour, minute);
+    // Program Identification
+    rdsparser_pi_t pi = rdsparser_get_pi(rds_parser);
+    if (pi != RDSPARSER_PI_UNKNOWN) {
+        printf("RDS - PI: %04X", pi);
     } else {
-        printf("   Date/Time: Not available\n");
+        printf("RDS - PI: Unknown");
+    }
+    
+    // Program Type
+    rdsparser_pty_t pty = rdsparser_get_pty(rds_parser);
+    if (pty != RDSPARSER_PTY_UNKNOWN) {
+        printf(", PTY: %s (%d)", rdsparser_pty_lookup_long(pty, false), pty);
+    } else {
+        printf(", PTY: Unknown");
+    }
+    
+    // Traffic Program and Announcement
+    rdsparser_tp_t tp = rdsparser_get_tp(rds_parser);
+    if (tp != RDSPARSER_TP_UNKNOWN) {
+        printf(", TP: %d", tp);
+    }
+    
+    rdsparser_ta_t ta = rdsparser_get_ta(rds_parser);
+    if (ta != RDSPARSER_TA_UNKNOWN) {
+        printf(", TA: %d", ta);
+    }
+    
+    // Music/Speech
+    rdsparser_ms_t ms = rdsparser_get_ms(rds_parser);
+    if (ms != RDSPARSER_MS_UNKNOWN) {
+        printf(", MS: %d", ms);
+    }
+    
+    printf("\n");
+    
+    // Program Service Name
+    const rdsparser_string_t *ps = rdsparser_get_ps(rds_parser);
+    if (ps && rdsparser_string_get_available(ps)) {
+        printf("      PS: %s\n", rdsparser_string_get_content(ps));
+    } else {
+        printf("      PS: Not available\n");
+    }
+    
+    // Radio Text
+    const rdsparser_string_t *rt_a = rdsparser_get_rt(rds_parser, RDSPARSER_RT_FLAG_A);
+    if (rt_a && rdsparser_string_get_available(rt_a)) {
+        printf("      RT-A: %s\n", rdsparser_string_get_content(rt_a));
+    }
+    
+    const rdsparser_string_t *rt_b = rdsparser_get_rt(rds_parser, RDSPARSER_RT_FLAG_B);
+    if (rt_b && rdsparser_string_get_available(rt_b)) {
+        printf("      RT-B: %s\n", rdsparser_string_get_content(rt_b));
+    }
+    
+    // Clock Time - use the data stored from the callback
+    if (ct_data.valid) {
+        printf("      Date: %02u/%02u/%04u\n", ct_data.day, ct_data.month, ct_data.year);
+        printf("      Time: %02u:%02u\n", ct_data.hour, ct_data.minute);
+    } else {
+        printf("      Date/Time: Not available\n");
     }
 }
+
 static void set_frequency(float frequency)
 {
     fm_set_frequency_blocking(&radio, frequency);
     printf("Set frequency: %.2f MHz\n", fm_get_frequency(&radio));
-    rds_parser_reset(&rds_parser);
+    rdsparser_clear(rds_parser);
+    
+    // Reset CT data
+    ct_data.valid = false;
 }
 
 static void seek(fm_seek_direction_t direction)
@@ -327,5 +450,8 @@ static void seek(fm_seek_direction_t direction)
     } else {
         printf("... failed: %d\n", progress.result);
     }
-    rds_parser_reset(&rds_parser);
+    rdsparser_clear(rds_parser);
+    
+    // Reset CT data
+    ct_data.valid = false;
 }
